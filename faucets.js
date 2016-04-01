f0390c14ec74296f206a37d02d9e560db7c38cc3
SW911 <sw911@inbox.ru>
2016-04-01 15:02:03 +0200

    Update faucets.js
    explaination

--- faucets.js
+++ faucets.js
@@ -1,2 +1,9 @@
-﻿var walletId = "32etfEUfSpdvaYxq9mbRG7EQLWYhntYCPS";
+﻿var walletId = ""; // Your Wallet id, will be displayed for easier access
 var faucets = new Array();
+
+var obj = {
+    url : "http://", // this is the URL where the BitCoin Faucet is located
+    name : "",       // this is the name of button.
+    timer : "",      // the time between Claims.
+    iframe : "true"  // some faucets dont allow to be displayed in an iFrame, this is configured here
+};
